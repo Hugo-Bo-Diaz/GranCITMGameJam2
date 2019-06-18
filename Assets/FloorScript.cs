4da5ef5df9ea1274b7e205de040e0cb92bf6b814
Lorién <lorienportella@gmail.com>
2019-06-18 17:04:54 +0200

    player controller still wip

---
+++ Assets/FloorScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorScript : MonoBehaviour
+{
+    public bool isTeamAlga = false;
+    public GameController gameController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.collider.tag == "Ball")
+        {
+            gameController.TeamScored(!isTeamAlga);
+        }
+
+    }
+}
