2cebc562cc23b4bbfdaea02de21e1ec0be727f60
Lorién <lorienportella@gmail.com>
2019-06-18 12:12:12 +0200

    ball physics first iteration

---
+++ Assets/BubblePhysics.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubblePhysics : MonoBehaviour
+{
+
+    public float max_speed = 10.0f;
+    public float min_speed = 0.1f;
+    public float water_resistance = 0.1f;
+    public float gravity_magnitude = 0.0f;
+
+    float current_speed = 0.5f;
+    
+    Vector2 direction = new Vector2(0f, 0f);
+    Vector2 test_hit = new Vector2(-0.5f, -1f);
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 new_direction = direction.normalized * current_speed + new Vector2(0f, 1f) * gravity_magnitude;
+        current_speed -= water_resistance;
+        current_speed = Mathf.Clamp(current_speed, min_speed, max_speed);
+        float magnitude = Mathf.Clamp(new_direction.magnitude, min_speed, max_speed);
+        direction = new_direction.normalized * magnitude;
+        Vector3 new_position = new Vector3(transform.position.x + direction.x, transform.position.y + direction.y, transform.position.z);
+        transform.SetPositionAndRotation(new_position, transform.rotation);
+
+        if (Input.GetKeyDown("space"))
+        {
+            direction = test_hit;
+            current_speed = max_speed;
+        }
+    }
+
+    void ApplyHit(Vector2 new_direction, float speed)
+    {
+        direction = new_direction;
+        current_speed = speed;
+    }
+}
