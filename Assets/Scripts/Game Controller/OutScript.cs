983c7da6e35caba11538e64a42ae162722d3af96
Lorién <lorienportella@gmail.com>
2019-06-19 03:17:14 +0200

    ball fucking explodes

--- Assets/Scripts/Game Controller/OutScript.cs
+++ Assets/Scripts/Game Controller/OutScript.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class OutScript : MonoBehaviour
 {
     public GameController gameController;
+    public GameObject prefab;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Ball")
         {
             gameController.TeamScored(gameController.LastTouchedPoint(), "floor");
+            Instantiate(prefab, collision.transform.position, collision.transform.rotation);
+            collision.transform.position = new Vector3(100000, 100000, collision.transform.position.z);
         }
     }
 }
