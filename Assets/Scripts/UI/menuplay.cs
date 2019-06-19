44cafe12883e4f8f05293157e259e1bce922192f
Hugo-Bo-Diaz <hugobodiaz@gmail.com>
2019-06-19 11:52:39 +0200

    refined main menu

--- Assets/Scripts/UI/menuplay.cs
+++ Assets/Scripts/UI/menuplay.cs
@@ -6,14 +6,37 @@ public class menuplay : MonoBehaviour
 {
     // Start is called before the first frame update
 
+	public bool controlsareopen = false;
+	public GameObject controls;
+
     public void changemenuscene(string menuplay)
     {
             Application.LoadLevel (menuplay);
     }
 
+	public void opencontrolsmenu()
+	{
+		controls.SetActive(true);
+		controlsareopen=true;
+	}
+
+	public void closecontrolsmenu()
+	{
+		controls.SetActive(false);
+		controlsareopen=false;
+	}
+
+	public void CloseGame()
+	{
+		Application.Quit();
+	}
+
     // Update is called once per frame
     void Update()
     {
-        
+		if (controlsareopen == true && Input.anyKey)
+		{
+			closecontrolsmenu();
+		}
     }
 }
